27a98f749cde315eb63736672466618250e63a06
Lawrence Fiesta <lfiesta86@gmail.com>
2019-05-31 21:11:57 -0700

    figured a way to not display commas, only to accept a - z input, added pictures for when you guess the word correctly/incorrectly. Not able to figure out how to get the audio to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,117 +10,122 @@ var letterGuessed = []; //needs a reset
 var charLoc = []; //needs a reset
 var correctGuess = 0; //needs a reset
 var winT = document.getElementById("winText");
+var loseT = document.getElementById("loseText");
 var patternT = document.getElementById("patternText");
 var guessNumT = document.getElementById("guessNumText");
 var lettersT = document.getElementById("lettersText");
-//winText, patternText, guessNumText, lettersText
-play();
-//create a function method to start from scratch and encapsulate 
-//if playing again reset sushi choice, length, and underscores, remove word from dictionary 
+var wordT = document.getElementById("wordChoice");
+var imgT = document.getElementById("pics");
 
-function play(){
+playG();
+
+
+document.onkeyup = function(event){
+  //only accepts lower case a - z 
+  if(event.keyCode >= 65 && event.keyCode <= 90){
+    //forces guesses to be lower case
+    var guesses = event.key.toLowerCase();    
+    //on start up refreshes the website with the stats
+    gameStats();
+    
+    //checks to see if the guessed letter is in an array called guesses if not then add to array
+    if (letterGuessed.indexOf(guesses) === -1){
+      letterGuessed.push(guesses);
+      check(guesses);
+      replace(guesses);
+      checkStatus();
+      
+    } else {
+      wordT.textContent = "You already selected  " + guesses;
+      
+    }
+    gameStats();
+  } else {
+    wordT.textContent = "You selected an invalid option " + event.key;
+  }
+}
+
+function playG(){
   sushiChoice = sushiDictionary[Math.floor(Math.random() * sushiDictionary.length)];
   
   sushiLength = sushiChoice.length;
   guessRemain = (sushiLength);
-
-  //recursive underscores and stores them in an array
-  underScores(sushiLength);
   
+  underScores(sushiLength);  
 }
+//creates the pattern of under scores and stores it in an array
 function underScores(x){
   if (x === 0) {
     return;
   }
   underScores(--x) + pattern.push("_");
 }
-//meat of the game #Guesses (sushiLength + 3), #guessesremaining, lettersGuessed, win/lose 
-
-document.onkeyup = function(event){
-
-  var guesses = event.key;
-  gameStats();
-
-  if (letterGuessed.indexOf(guesses) === -1){
-    letterGuessed.push(guesses);
-
-    //check function
-    check(guesses);
-    //replace _ with guess resets charLoc array to empty
-    replace(guesses);
-    // console.log("char loc " + charLoc);
-    checkStatus();
-
-  } else {
-    console.log("you already used that letter " + guesses);
-
-  }
-  gameStats();
-
-}
-
+//checks to see if the letter is part of the word
 function check(letter){
-
   for(var i = 0; i < sushiLength; i++){
     if (letter === sushiChoice.charAt(i)) {
       charLoc.push(i);
       correctGuess++;
     }
   }
-
   if(charLoc.length === 0){
-    guessRemain--;
-
+    guessRemain--;      
   }
-  
 }
 
+//replace the "_" with the correctly guessed letter
 function replace(letter){
-
   for(var j = 0; j < charLoc.length; j++){
     pattern[charLoc[j]] = letter;
   } 
   charLoc.length = 0;
-
 }
 
+//checks the status of the current game to see if it's a game over or not
 function checkStatus(){
   if (correctGuess === sushiLength) {
-    //gameOver and resets arrays
     gameOver(1);
 
   } else if (guessRemain === 0) {
-    //gameOver and resets arrays
-
     gameOver(-1);
   }
 }
 
+//increments the win/lose and display the attempted word on screen.
 function gameOver(x){
-
   if(x === 1){
-    console.log("you win!");
     win++;
+    imgT.setAttribute("src", "http://www.randomkittengenerator.com/cats/rotator.php");
 
   } else if (x === -1){
-    console.log("you lose!");
     lose++;
+    imgT.setAttribute("src", "http://www.hereinreality.com/funny_pictures/randim.php");
+    if(lose === 3){
+      alert("Just quit... go and do something productive.");
+    }
   }
-  reset();
-
+  wordT.textContent = sushiChoice + " roll";
+  setTimeout(function(){
+    reset();
+  }, 2000);
 }
 
+//resets the variables to start the next game
 function reset(){
   pattern.length = 0;
   guessRemain = 0;
   letterGuessed.length = 0;
   correctGuess = 0;
-  play();
+  wordT.textContent = "Play!";
+  imgT.removeAttribute("src");
+  playG();
 }
 
+//grabs the game stats to show it on website
 function gameStats(){
   winT.textContent = win;
-  patternT.textContent = pattern;
+  loseT.textContent = lose;
+  patternT.textContent = pattern.join(" ");
   guessNumT.textContent = guessRemain;
-  lettersT.textContent = letterGuessed;
+  lettersT.textContent = letterGuessed.join(", ");
 }
